4a14e2cc725d624bf531966df4e6eff1ff38fa40
PresidentSam100 <56271444+PresidentSam100@users.noreply.github.com>
2022-04-21 14:07:07 -0500

    Update MainFile.java

--- Priority Queue & Tester/src/MainFile.java
+++ Priority Queue & Tester/src/MainFile.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 
 public class MainFile
 {
-    /*
-    Sam Lu
-    Mr. Tully
-    Period 4
-    */
-
     public static void main(String [] args)
     {
         Scanner scanner = new Scanner(System.in);
