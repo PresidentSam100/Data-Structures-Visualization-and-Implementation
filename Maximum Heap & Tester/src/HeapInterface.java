0b1a2b26317c4a1c064f988b169355df14b9d4e3
PresidentSam100 <56271444+PresidentSam100@users.noreply.github.com>
2022-04-21 14:26:36 -0500

    Update HeapInterface.java

--- Maximum Heap & Tester/src/HeapInterface.java
+++ Maximum Heap & Tester/src/HeapInterface.java
@@ -1,11 +1,5 @@
 public interface HeapInterface <E extends Comparable>
 {
-    /*
-    Sam Lu
-    Mr. Tully
-    Period 4
-    */
-
     public boolean add(E item);
 
     public boolean insert(E item);
